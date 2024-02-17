8fbd356455fef3ef2037c458c6e3edb133f712ea
okay-head <68184623+okay-head@users.noreply.github.com>
2024-02-17 19:19:30 +0530

    project deserted

--- public/index.php
+++ public/index.php
@@ -1,11 +1,32 @@
 <?php require 'shared/header.php';?>
-<?php require 'shared/footer.php';?>
+<?php require 'shared/toast.php';?>
 
 
 <?php 
-$sql = "INSERT INTO `agency` (`agency_id`, `agency_name`, `email`, `password`, `phone`, `address`, `cars_owned`, `cars_booked`) VALUES (NULL, \'cars101\', \'cars@101.com\', \'cars101\', \'1122334455\', \'123, hauz khaz, delhi\', \'{\\r\\n\\\"id\\\": 1,\\r\\n\\\"name\\\": \\\"Mclaren\\\"\\r\\n}\', \'{\\r\\n\\\"id\\\": 1,\\r\\n\\\"name\\\": \\\"Mclaren\\\"\\r\\n}\');";
+ $agencyName = 'cars201';
+ $email = 'cars201@email.com';
+ $password = 'cars201';
+ $phone = 1234567891;
+ $address = '201, cars, newDelhi';
 
-$sql2 = "SELECT * FROM `agency`";
+$sql = "INSERT INTO `agency` (`agency_id`, `agency_name`, `email`, `password`, `phone`, `address`, `cars_owned`, `cars_booked`) VALUES (NULL,'$agencyName', '$email', '$password', '$phone', '$address', NULL, NULL)";
 
+$sql2 = 'SELECT * FROM `agency`';
 
-?>
+try {
+  $result = $conn->query($sql2);
+  if ($conn->query($sql) == true) {
+    toast('New record created successfully');
+    foreach(($result->fetch_assoc()) as $key=>$val)
+    echo $key.':'.$val.'<br>';
+
+    }
+  } catch (Exception $th) {
+    // die('Connection failed: ' . $conn->connect_error);
+    echo $th;
+    $err = 'ERROR<br>' . substr($th, 0, 50) . '...';
+    toast($err);
+  }
+  ?>
+
+<?php require 'shared/footer.php';?>
